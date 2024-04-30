0f8d1a5ed75de15fdc1077ed45b50c80bf4e0906
Charles Hall <charles@computer.surgery>
2024-04-30 16:00:19 -0700

    factor out nix code into new files via `makeScope`
    This makes the Nix code a lot easier to reason about.

---
+++ nix/shell.nix
@@ -0,0 +1,34 @@
+# Keep sorted
+{ default
+, engage
+, inputs
+, jq
+, mkShell
+, system
+, toolchain
+}:
+
+mkShell {
+  env = default.env // {
+    # Rust Analyzer needs to be able to find the path to default crate
+    # sources, and it can read this environment variable to do so. The
+    # `rust-src` component is required in order for this to work.
+    RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+  };
+
+  # Development tools
+  nativeBuildInputs = [
+    # Always use nightly rustfmt because most of its options are unstable
+    #
+    # This needs to come before `toolchain` in this list, otherwise
+    # `$PATH` will have stable rustfmt instead.
+    inputs.fenix.packages.${system}.latest.rustfmt
+
+    # Keep sorted
+    engage
+    jq
+    toolchain
+  ]
+  ++
+  default.nativeBuildInputs;
+}
