17cb7732bd595cac2a33c956b2365b7c8bcbae04
Charles Hall <charles@computer.surgery>
2024-04-30 14:45:45 -0700

    flatten flake inputs

--- flake.nix
+++ flake.nix
@@ -3,19 +3,9 @@
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils?ref=main";
     nix-filter.url = "github:numtide/nix-filter?ref=main";
-    flake-compat = {
-      url = "github:edolstra/flake-compat?ref=master";
-      flake = false;
-    };
-
-    fenix = {
-      url = "github:nix-community/fenix?ref=main";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    crane = {
-      url = "github:ipetkov/crane?ref=master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-compat = { url = "github:edolstra/flake-compat?ref=master"; flake = false; };
+    fenix = { url = "github:nix-community/fenix?ref=main"; inputs.nixpkgs.follows = "nixpkgs"; };
+    crane = { url = "github:ipetkov/crane?ref=master"; inputs.nixpkgs.follows = "nixpkgs"; };
     attic.url = "github:zhaofengli/attic?ref=main";
   };
 
