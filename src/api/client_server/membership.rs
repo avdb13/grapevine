a60a9551e1bbbf99888a80ae1a4fc32b0afdabd0
Charles Hall <charles@computer.surgery>
2024-05-20 21:37:39 -0700

    Revert "Merge branch 'check-if-membership-is-case-endpoints' into 'next'"
    This reverts commit 7ace9b0dff12caa1a69722b77aa39ab3e57f7903, reversing
    changes made to 624654a88b593d231029d7a209dfabd9ccc39bcb.

--- src/api/client_server/membership.rs
+++ src/api/client_server/membership.rs
@@ -202,12 +202,6 @@ pub(crate) async fn kick_user_route(
 ) -> Result<Ra<kick_user::v3::Response>> {
     let sender_user = body.sender_user.as_ref().expect("user is authenticated");
 
-    if let Ok(true) =
-        services().rooms.state_cache.is_left(sender_user, &body.room_id)
-    {
-        return Ok(Ra(kick_user::v3::Response {}));
-    }
-
     let mut event: RoomMemberEventContent = serde_json::from_str(
         services()
             .rooms
@@ -270,14 +264,6 @@ pub(crate) async fn ban_user_route(
     body: Ar<ban_user::v3::Request>,
 ) -> Result<Ra<ban_user::v3::Response>> {
     let sender_user = body.sender_user.as_ref().expect("user is authenticated");
-
-    if let Ok(Some(membership_event)) =
-        services().rooms.state_accessor.get_member(&body.room_id, sender_user)
-    {
-        if membership_event.membership == MembershipState::Ban {
-            return Ok(Ra(ban_user::v3::Response {}));
-        }
-    }
 
     let event = services()
         .rooms
@@ -354,14 +340,6 @@ pub(crate) async fn unban_user_route(
     body: Ar<unban_user::v3::Request>,
 ) -> Result<Ra<unban_user::v3::Response>> {
     let sender_user = body.sender_user.as_ref().expect("user is authenticated");
-
-    if let Ok(Some(membership_event)) =
-        services().rooms.state_accessor.get_member(&body.room_id, sender_user)
-    {
-        if membership_event.membership != MembershipState::Ban {
-            return Ok(Ra(unban_user::v3::Response {}));
-        }
-    }
 
     let mut event: RoomMemberEventContent = serde_json::from_str(
         services()
@@ -545,14 +523,6 @@ async fn join_room_by_id_helper(
     _third_party_signed: Option<&ThirdPartySigned>,
 ) -> Result<join_room_by_id::v3::Response> {
     let sender_user = sender_user.expect("user is authenticated");
-
-    if let Ok(true) =
-        services().rooms.state_cache.is_joined(sender_user, room_id)
-    {
-        return Ok(join_room_by_id::v3::Response {
-            room_id: room_id.into(),
-        });
-    }
 
     let mutex_state = Arc::clone(
         services()
