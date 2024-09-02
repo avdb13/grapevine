f1869aad79ae6abade00963936c390ed95315812
avdb13 <avdb@keemail.me>
2024-09-02 03:48:22 +0200

    feat: single sign-on provider

--- src/service/sso.rs
+++ src/service/sso.rs
@@ -7,12 +7,18 @@ use http::{header::USER_AGENT, HeaderValue};
 use mas_http::{
     BodyToBytesResponseLayer, BytesToBodyRequestLayer, HttpService,
 };
+use mas_oidc_client::{
+    requests::discovery, types::oidc::VerifiedProviderMetadata,
+};
 use tower::{buffer::Buffer, ServiceBuilder, ServiceExt};
 use tower_http::ServiceBuilderExt;
+use tracing::error;
+
+use crate::{config::ProviderConfig, services, Error, Result};
 
 pub(crate) struct Service {
     pub(crate) db: &'static dyn Data,
-    pub(crate) http_service: HttpService,
+    pub(crate) client: HttpService,
 }
 
 impl Service {
@@ -24,13 +30,57 @@ impl Service {
 
         let service = ServiceBuilder::new()
             .layer(BytesToBodyRequestLayer)
-            .layer(BodyToBytesResponseLayer)
-            .override_request_header(USER_AGENT, user_agent)
-            .service(mas_http::make_untraced_client());
+            .layer(BodyToBytesResponseLayer);
 
         Arc::new(Self {
             db,
-            http_service: HttpService::new(Buffer::new(service.boxed(), 16)),
+            client: HttpService::new(Buffer::new(
+                service
+                    .override_request_header(USER_AGENT, user_agent)
+                    .service(mas_http::make_untraced_client())
+                    .boxed(),
+                16,
+            )),
         })
     }
 }
+
+#[derive(Clone, Debug)]
+pub(crate) struct Provider {
+    pub(crate) config: &'static ProviderConfig,
+    pub(crate) metadata: VerifiedProviderMetadata,
+}
+
+impl Provider {
+    pub(crate) async fn fetch_metadata(
+        config: &'static ProviderConfig,
+    ) -> Result<Self> {
+        match discovery::discover(&services().sso.client, &config.issuer).await
+        {
+            Ok(metadata) => Ok(Provider {
+                config,
+                metadata,
+            }),
+            Err(error) => {
+                error!(
+                    %error,
+                    provider = %config.inner.id,
+                    "Failed to fetch identity provider metadata",
+                );
+
+                Err(Error::bad_config(
+                    "Failed to fetch identity provider metadata. Please check \
+                     your config.",
+                ))
+            }
+        }
+    }
+}
+
+impl PartialEq for Provider {
+    fn eq(&self, other: &Self) -> bool {
+        self.config == other.config
+    }
+}
+
+impl Eq for Provider {}
